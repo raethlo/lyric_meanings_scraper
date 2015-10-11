3463b811909904488006c31565f5db6cc6628afb
raethlo <roman.rostar@gmail.com>
2015-10-11 18:30:11 +0200

    initial commit

--- scraper.rb
+++ scraper.rb
@@ -23,3 +23,52 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+require 'scraperwiki'
+require 'faraday'
+require 'nokogiri'
+
+def open_page(id)
+  url = "http://songmeanings.com/songs/view/#{id}"
+
+  html = Faraday.get url
+  doc = Nokogiri.HTML html.body
+end
+
+def scrape_lyric(doc)
+  bread = doc.css('ul.breadcrumbs')
+  navigation = bread.css('li')
+
+  _, artist, album, song = navigation.map { |li| li.text.strip }
+
+  lyrics_box = doc.css('div.lyric-box')
+  lyrics_box.children.css('div').remove
+
+  lyrics = lyrics_box.text
+
+  {
+      artist: artist,
+      album: album,
+      song: song,
+      lyrics: lyrics
+  }
+end
+
+#
+i = 1
+
+10.times do
+  page = open_page(i)
+  data = scrape_lyric(page)
+
+  ScraperWiki.save_sqlite(
+    ["lyrics"],
+    {
+        "artist" => data[:artist],
+        "album" => data[:album],
+        "lyrics" => data[:lyrics]
+    }
+  )
+
+  i += 1
+end
